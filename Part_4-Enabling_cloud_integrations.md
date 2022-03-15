bcd9cd2b62419f7e94b82570aa1794544cca1fdb
Samuel <samuel@sava.be>
2022-03-15 10:00:37 +0100

    chore: wip part 4

--- Part_4-Enabling_cloud_integrations.md
+++ Part_4-Enabling_cloud_integrations.md
@@ -1,1 +1,7 @@
 # Enable cloud integrations to monitor the AWS environment through Terraform with New Relic
+
+The New Relic Cloud integrations are a great way for you to get some great insights into their AWS environment, and link that data to APM, Browser, or even Infrastructure data. The only downside is that it's quite a hassle to set it all up, especially if you have to setup multiple New Relic and/or AWS accounts. Luckily the Observability as Code team has recently released the final bits to make this entire configuration fully automated.
+
+During this part of the workshop we will go through each step on how to set up the Cloud integrations end to end. Now because this requires pretty extensive permissions on the AWS side we will review the code, and if you wish you can test with your own AWS account. The code below is everything a customer would need to get started.
+
+
---
+++ reference/Part_4/cloud-integration.tf
@@ -0,0 +1,200 @@
+variable "NEW_RELIC_ACCOUNT_ID" {
+  type = string
+}
+
+// We generate a random string for use in a couple of resources so you don't have collisions with other users
+resource "random_string" "user" {
+  length  = 6
+  upper   = false
+  lower   = true
+  number  = true
+  special = false
+}
+
+data "aws_iam_policy_document" "newrelic_assume_policy" {
+  statement {
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "AWS"
+      identifiers = [754728514883]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "sts:ExternalId"
+      values   = [var.NEW_RELIC_ACCOUNT_ID]
+    }
+  }
+}
+
+resource "aws_iam_role" "newrelic_aws_role" {
+  name               = "NewRelicInfrastructure-Integrations-${random_string.user.result}"
+  description        = "New Relic Cloud integration role"
+  assume_role_policy = data.aws_iam_policy_document.newrelic_assume_policy.json
+}
+
+resource "aws_iam_policy" "newrelic_aws_permissions" {
+  name        = "NewRelicCloudStreamReadPermissions-${random_string.user.result}"
+  description = ""
+  policy      = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": [
+        "budgets:ViewBudget",
+        "cloudtrail:LookupEvents",
+        "config:BatchGetResourceConfig",
+        "config:ListDiscoveredResources",
+        "ec2:DescribeInternetGateways",
+        "ec2:DescribeVpcs",
+        "ec2:DescribeNatGateways",
+        "ec2:DescribeVpcEndpoints",
+        "ec2:DescribeSubnets",
+        "ec2:DescribeNetworkAcls",
+        "ec2:DescribeVpcAttribute",
+        "ec2:DescribeRouteTables",
+        "ec2:DescribeSecurityGroups",
+        "ec2:DescribeVpcPeeringConnections",
+        "ec2:DescribeNetworkInterfaces",
+        "ec2:DescribeVpnConnections",
+        "health:DescribeAffectedEntities",
+        "health:DescribeEventDetails",
+        "health:DescribeEvents",
+        "tag:GetResources",
+        "xray:BatchGet*",
+        "xray:Get*"
+      ],
+      "Effect": "Allow",
+      "Resource": "*"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "newrelic_aws_policy_attach" {
+  role       = aws_iam_role.newrelic_aws_role.name
+  policy_arn = aws_iam_policy.newrelic_aws_permissions.arn
+}
+
+resource "newrelic_cloud_aws_link_account" "newrelic_cloud_integration" {
+  arn = aws_iam_role.newrelic_aws_role.arn
+  metric_collection_mode = "PUSH"
+  name = "production-${random_string.user.result}"
+  depends_on = [aws_iam_role_policy_attachment.newrelic_aws_policy_attach]
+}
+
+resource "newrelic_api_access_key" "newrelic_aws_access_key" {
+  account_id  = var.NEW_RELIC_ACCOUNT_ID
+  key_type    = "INGEST"
+  ingest_type = "LICENSE"
+  name        = "Ingest License key"
+  notes       = "AWS Cloud Integrations Firehost Key"
+}
+
+resource "aws_iam_role" "firehose_newrelic_role" {
+  name = "firehose_newrelic_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "firehose.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_s3_bucket" "newrelic_aws_bucket" {
+  bucket = "newrelic-aws-bucket-${random_string.user.result}"
+}
+
+resource "aws_s3_bucket_acl" "newrelic_aws_bucket_acl" {
+  bucket = aws_s3_bucket.newrelic_aws_bucket.id
+  acl    = "private"
+}
+
+resource "aws_kinesis_firehose_delivery_stream" "newrelic_firehost_stream" {
+  name        = "newrelic_firehost_stream"
+  destination = "http_endpoint"
+
+  s3_configuration {
+    role_arn           = aws_iam_role.firehose_newrelic_role.arn
+    bucket_arn         = aws_s3_bucket.newrelic_aws_bucket.arn
+    buffer_size        = 10
+    buffer_interval    = 400
+    compression_format = "GZIP"
+  }
+
+  http_endpoint_configuration {
+    url                = "https://aws-api.newrelic.com/cloudwatch-metrics/v1" # US
+    # url                = "https://aws-api.eu01.nr-data.net/cloudwatch-metrics/v1" # EU
+    name               = "New Relic"
+    access_key         = newrelic_api_access_key.newrelic_aws_access_key.key
+    buffering_size     = 1
+    buffering_interval = 60
+    role_arn           = aws_iam_role.firehose_newrelic_role.arn
+    s3_backup_mode     = "FailedDataOnly"
+
+    request_configuration {
+      content_encoding = "GZIP"
+    }
+  }
+}
+
+resource "aws_iam_role" "metric_stream_to_firehose" {
+  name = "metric_stream_to_firehose_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": "streams.metrics.cloudwatch.amazonaws.com"
+      },
+      "Effect": "Allow",
+      "Sid": ""
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy" "metric_stream_to_firehose" {
+  name = "default"
+  role = aws_iam_role.metric_stream_to_firehose.id
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "firehose:PutRecord",
+                "firehose:PutRecordBatch"
+            ],
+            "Resource": "${aws_kinesis_firehose_delivery_stream.newrelic_firehost_stream.arn}"
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_cloudwatch_metric_stream" "newrelic_metric_stream" {
+  name          = "newrelic-metric-stream"
+  role_arn      = aws_iam_role.metric_stream_to_firehose.arn
+  firehose_arn  = aws_kinesis_firehose_delivery_stream.newrelic_firehost_stream.arn
+  output_format = "opentelemetry0.7"
+}
